5aa3a06f8f306c86b465d8095313cc297e9b1302
jeremiasrg <jeremiasrg@gmail.com>
2021-05-01 19:27:12 -0300

    ajustes do builder

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header(props) {
   };
 
   function scrollFunction() {
-    if (document.documentElement.scrollTop > 90) {
+    if (document.documentElement.scrollTop > 30) {
       setNavbarShrink(true);
       console.log("true");
     } else {
